c743f90faa5d2534ef6e870ee35c0398ca6c84af
dor893 <dor.benartzi@postpost.runi.ac.il>
2024-11-27 15:57:41 +0200

    anagram - solved

--- Anagram.java
+++ Anagram.java
@@ -29,9 +29,9 @@ public class Anagram {
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
 
-		String oneString = preProcess(str1);
+		String oneString = preProcesswithoutspaces(str1);
 
-		String twoString = preProcess(str2);
+		String twoString = preProcesswithoutspaces(str2);
 
 		boolean anagram = false;
 
@@ -40,22 +40,29 @@ public class Anagram {
 			anagram = true;
 		}
 
-		else if (oneString.length() != twoString.length()) {
+		if (oneString.length() != twoString.length()) {
 
 			anagram = false;
 
 		} else {
+
+			for(int i = 0; i < oneString.length(); i++){
+
+				char charOne = oneString.charAt(i);
 		
-		    for(int i = 0; i < oneString.length(); i++ ) {
+		     while (twoString.length() > 0) {
 
 			    for(int t = 0; t < twoString.length(); t++) {
 
-				    if(str1.indexOf(twoString.charAt(t)) != -1) {
+				    if(twoString.charAt(t) == charOne) {
+
+						twoString = twoString.substring(0, t) + twoString.substring(t + 1); // delete the tested char
 
 					    anagram = true;
 
 				    }
 			    }
+		     }
 		    }
 		}
 
@@ -65,7 +72,7 @@ public class Anagram {
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
-	public static String preProcess(String str) {
+	public static String preProcess(String str) { 
 
 		String newString = "";
 
@@ -73,36 +80,53 @@ public class Anagram {
 
 			char c = str.charAt(i);
 
-			int ascii = c;
+			int asciiOfchar = c;
 
-			if(c == ' ' || c == '?' || c == '!') {
-
-				if(i + 1 < str.length()) {
-
-				newString += str.charAt(i + 1); 
-
-			    }
-
-			} else {
-
-				if(ascii >= 65 && ascii <= 90) {
+				if(asciiOfchar >= 65 && asciiOfchar <= 90) { // upper case
 
 					c = (char) (c + 32);
+
+					newString += c;
+				
+				} else if ((asciiOfchar >= 97 && asciiOfchar <= 122) || asciiOfchar == 32) { //lower case or tab
+
+					newString += c;
 				}
-
-				newString += c; 
-
-			}
 		}
+	return newString;
+	
+	} 
 
-		return newString;
+	public static String preProcesswithoutspaces(String str) { 
+
+		String newString = "";
+
+		for(int i = 0; i < str.length(); i++) {
+
+			char c = str.charAt(i);
+
+			int asciiOfchar = c;
+
+				if(asciiOfchar >= 65 && asciiOfchar <= 90) { // upper case
+
+					c = (char) (c + 32);
+
+					newString += c;
+				
+				} else if (asciiOfchar >= 97 && asciiOfchar <= 122) { //lower case or tab
+
+					newString += c;
+				}
+		}
+	return newString;
+	
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
 		
-		String remainingString = preProcess(str);
+		String remainingString = preProcesswithoutspaces(str);
 
 		String anagram = "";
 
